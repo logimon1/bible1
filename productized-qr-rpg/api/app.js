23495c4c32bfcdc0a75bf10be1ac2e52e82886e3
logimon1 <ksr700806@gmail.com>
2026-07-12 13:58:07 +0900

    Use read-only request paths for productized API

--- productized-qr-rpg/api/app.js
+++ productized-qr-rpg/api/app.js
@@ -1,5 +1,9 @@
 const { handleApiRequest } = require("../server/api");
+const { runWithAction } = require("../server/request-context");
 
 module.exports = async function appApi(req, res) {
-  await handleApiRequest(req, res);
+  const host = req.headers.host || "localhost";
+  const url = new URL(req.url, `http://${host}`);
+  const action = url.searchParams.get("action") || "";
+  await runWithAction(action, () => handleApiRequest(req, res));
 };
